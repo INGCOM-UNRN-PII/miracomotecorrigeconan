cddd51ccc6438c072eb41e995389991679eab6db
Martín René <martinvilu@gmail.com>
2024-03-19 10:12:58 -0300

    ajustes en las direcciones de los stylesheets, queda pendiente agregar separadores entre las partes del informe

--- dredd.sh
+++ dredd.sh
@@ -10,14 +10,15 @@ echo "ejecutando verificaciones con gradle wrapper"
 ./tp2-2024-submissions/$1/gradlew -p tp2-2024-submissions/$1 check
 
 echo "creacion del informe"
-xsltproc markdown.xsl tp2-2024-submissions/$1/build/reports/checkstyle/main.xml | sed s@$PWD\/@@ - > mensaje.md
-# xsltproc markdown.xsl tp2-2024-submissions/$1/build/reports/checkstyle/test.xml | sed s@$PWD\/@@ - >> mensaje.md
+xsltproc stylesheets/checkstyle.xsl tp2-2024-submissions/$1/build/reports/checkstyle/main.xml | sed s@$PWD\/@@ - > mensaje.md
+# xsltproc stylesheets/checkstyle.xsl tp2-2024-submissions/$1/build/reports/checkstyle/test.xml | sed s@$PWD\/@@ - >> mensaje.md
 
-xsltproc pmd.xsl tp2-2024-submissions/$1/build/reports/pmd/main.xml | sed s@$PWD\/@@ - >> mensaje.md
-# xsltproc pmd.xsl tp2-2024-submissions/$1/build/reports/pmd/test.xml | sed s@$PWD\/@@ - >> mensaje.md
+xsltproc stylesheets/pmd.xsl tp2-2024-submissions/$1/build/reports/pmd/main.xml | sed s@$PWD\/@@ - >> mensaje.md
+# xsltproc stylesheets/pmd.xsl tp2-2024-submissions/$1/build/reports/pmd/test.xml | sed s@$PWD\/@@ - >> mensaje.md
 
-echo "carga del informe como comentario"
+
 cat mensaje.md
+echo "Informe listo en $1.md"
 #gh pr comment https://github.com/INGCOM-UNRN-PII/$1/pull/1 -F mensaje.md
 
 mv mensaje.md $1.md
