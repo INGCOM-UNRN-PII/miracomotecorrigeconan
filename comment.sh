01e0cbb19b8597b694f544657f2dc67c4529e510
Martín René <martinvilu@gmail.com>
2024-03-20 15:59:55 -0300

    verificando que lo que intentamos comentar, exista (esta fijo al TP2, hay que buscar la forma de mejorarlo)

--- comment.sh
+++ comment.sh
@@ -1,1 +1,10 @@
-gh pr comment https://github.com/INGCOM-UNRN-PII/$1/pull/1 -F $1.md
+#!/usr/bin/bash
+
+echo "Procesando comentarios de corrección para tp2-2024-submissions/$1"
+
+if [ -d "tp2-2024-submissions/$1" ]; then
+
+    gh pr comment https://github.com/INGCOM-UNRN-PII/$1/pull/1 -F $1.md
+    echo https://github.com/INGCOM-UNRN-PII/$1/pull/1/files
+    
+fi
