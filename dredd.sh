9e940fa5032123e502b8b329f57886e5b5d71d20
Martin Rene <martinvilu@gmail.com>
2024-03-24 00:23:45 -0300

    dredd ahora esta preparado para todos los TP, usando un argumento adicional

--- dredd.sh
+++ dredd.sh
@@ -1,39 +1,40 @@
 #!/usr/bin/bash
 
-echo "Procesando tp2-2024-submissions/$1"
+echo "Procesando $1-submissions/$2"
+repo="$1-submissions/$2"
 
-if [ -d "tp2-2024-submissions/$1" ]; then
-#    echo "Clonando el repositorio si no lo estaba para $1"
+if [ -d "$repo" ]; then
+#    echo "Clonando el repositorio si no lo estaba para $repo"
 #    git clone git@github.com:INGCOM-UNRN-PII/$1.git tp2-2024-submissions/$1
 
-    echo "git pull en el repositorio $1"
-    git -C tp2-2024-submissions/$1 pull
+    echo "git pull en el repositorio $repo"
+    git -C $repo pull
 
     echo "ejecutando verificaciones con gradle wrapper"
-    
-    cp -fv build.gradle.timeout tp2-2024-submissions/$1/build.gradle  # Hotfix para activar timeout
-    ./tp2-2024-submissions/$1/gradlew -p tp2-2024-submissions/$1 check
-    cp -fv build.gradle.base tp2-2024-submissions/$1/build.gradle
+
+#    cp -fv build.gradle.timeout $repo/build.gradle  # Hotfix para activar timeout
+    ./$repo/gradlew -p $repo check
+#    cp -fv build.gradle.base $repo/build.gradle     # Desactivamos el Hotfix para no bloquear los pull
 
     echo "creacion del informe"
 
     echo "# El Juez Dredd" > mensaje.md
     echo "## Checkstyle código" >> mensaje.md
-    xsltproc stylesheets/checkstyle.xsl tp2-2024-submissions/$1/build/reports/checkstyle/main.xml | sed s@$PWD\/@@ - >> mensaje.md
+    xsltproc stylesheets/checkstyle.xsl $repo/build/reports/checkstyle/main.xml | sed s@$PWD\/@@ - >> mensaje.md
 
     # echo "## Checkstyle tests" >> mensaje.md
-    # xsltproc stylesheets/checkstyle.xsl tp2-2024-submissions/$1/build/reports/checkstyle/test.xml | sed s@$PWD\/@@ - >> mensaje.md
+    # xsltproc stylesheets/checkstyle.xsl $repo/build/reports/checkstyle/test.xml | sed s@$PWD\/@@ - >> mensaje.md
 
     echo "## PMD código" >> mensaje.md
-    xsltproc stylesheets/pmd.xsl tp2-2024-submissions/$1/build/reports/pmd/main.xml | sed s@$PWD\/@@ - >> mensaje.md
-    # echo "## PMD código" >> mensaje.md
-    # xsltproc stylesheets/pmd.xsl tp2-2024-submissions/$1/build/reports/pmd/test.xml | sed s@$PWD\/@@ - >> mensaje.md
+    xsltproc stylesheets/pmd.xsl $repo/build/reports/pmd/main.xml | sed s@$PWD\/@@ - >> mensaje.md
+    # echo "## PMD tests" >> mensaje.md
+    # xsltproc stylesheets/pmd.xsl $repo/build/reports/pmd/test.xml | sed s@$PWD\/@@ - >> mensaje.md
 
     echo "## Conan ejecuta los Tests" >> mensaje.md
-    for filename in tp2-2024-submissions/$1/build/test-results/test/*.xml; do
+    for filename in $repo/build/test-results/test/*.xml; do
         xsltproc stylesheets/junit.xsl $filename >> mensaje.md
     done
 
-    echo "Informe listo en $1.md"
-    mv mensaje.md $1.md
+    echo "Informe listo en $2.md"
+    mv mensaje.md $2.md
 fi
