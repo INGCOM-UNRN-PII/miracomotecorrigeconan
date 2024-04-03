114e78340a93c409f17b1c1c46226ac9c1dfee5f
Martin Rene <martinvilu@gmail.com>
2024-04-02 23:25:17 -0300

    abre dos tabs de firefox directamente y en segundo plano

--- comment.sh
+++ comment.sh
@@ -3,8 +3,7 @@
 echo "Procesando comentarios de corrección para $1"
 
 if [ -f "$1.md" ]; then
-    gh pr comment https://github.com/INGCOM-UNRN-PII/$1/pull/1 -F $1.md
-    echo "https://github.com/INGCOM-UNRN-PII/$1/pull/1/files"
+    firefox -new-tab -url "https://github.com/INGCOM-UNRN-PII/$1/pull/1/files" -new-tab -url "`gh pr comment https://github.com/INGCOM-UNRN-PII/$1/pull/1 -F $1.md`"
 else
     echo "El reporte '$1.md' no fue encontrado"
     echo "Parece que dredd no pasó por acá aún."
