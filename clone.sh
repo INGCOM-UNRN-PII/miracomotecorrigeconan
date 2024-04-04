5943d07c99234475fd10de1fec85146cb8fd7d78
Martín René <martinvilu@gmail.com>
2024-04-04 15:49:00 -0300

    agregadas referencias a los ultimos TP

--- clone.sh
+++ clone.sh
@@ -3,3 +3,4 @@
 gh classroom clone student-repos -a 572999 #TP1
 gh classroom clone student-repos -a 576011 #TP2
 gh classroom clone student-repos -a 579743 #TP3
+gh classroom clone student-repos -a 584890 #TP4
--- pull.sh
+++ pull.sh
@@ -2,4 +2,6 @@
 
 gh classroom pull student-repos -a 572999 #TP1
 gh classroom pull student-repos -a 576011 #TP2
+gh classroom pull student-repos -a 579743 #TP3
+gh classroom pull student-repos -a 584890 #TP4
 
