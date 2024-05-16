8a78103454c0c91dc2da76e99c43e461ebcc0d1f
Martín René <martinvilu@gmail.com>
2024-05-16 09:54:54 -0300

    agregados ID's de los practicos 5 y 6

--- clone.sh
+++ clone.sh
@@ -4,3 +4,7 @@ gh classroom clone student-repos -a 572999 #TP1
 gh classroom clone student-repos -a 576011 #TP2
 gh classroom clone student-repos -a 579743 #TP3
 gh classroom clone student-repos -a 584890 #TP4
+gh classroom clone student-repos -a 595462 #TP6
+gh classroom clone student-repos -a 599537 #TP8
+
+
--- pull.sh
+++ pull.sh
@@ -4,4 +4,5 @@ gh classroom pull student-repos -a 572999 #TP1
 gh classroom pull student-repos -a 576011 #TP2
 gh classroom pull student-repos -a 579743 #TP3
 gh classroom pull student-repos -a 584890 #TP4
-
+gh classroom pull student-repos -a 595462 #TP5
+gh classroom pull student-repos -a 599537 #TP6
