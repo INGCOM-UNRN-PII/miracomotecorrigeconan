bd7df487e7d7cf880724a68ade6dec6b6bfdff0a
Martín René <martinvilu@gmail.com>
2024-06-26 09:04:18 -0300

    agregados ultimos ID's de practicas 2024

--- clone.sh
+++ clone.sh
@@ -7,4 +7,8 @@ gh classroom clone student-repos -a 584890 #TP4
 gh classroom clone student-repos -a 595462 #TP6
 gh classroom clone student-repos -a 599537 #TP8
 
+gh classroom clone student-repos -a 602302 #TP9
+gh classroom clone student-repos -a 609087 #TP10
+gh classroom clone student-repos -a 614010 #TP11
+
 
