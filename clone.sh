908335ab4e3ff377d4b9c588ef8805ed6255ed85
Martín René <martinvilu@gmail.com>
2025-03-12 10:28:07 -0300

    cambios en los assignments id del 2025

--- clone.sh
+++ clone.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
-gh classroom clone student-repos -a 572999 #TP1
-gh classroom clone student-repos -a 576011 #TP2
-gh classroom clone student-repos -a 579743 #TP3
-gh classroom clone student-repos -a 584890 #TP4
-gh classroom clone student-repos -a 595462 #TP6
-gh classroom clone student-repos -a 599537 #TP8
+gh classroom clone student-repos -a 772080 #TP1
+#gh classroom clone student-repos -a 576011 #TP2
+#gh classroom clone student-repos -a 579743 #TP3
+#gh classroom clone student-repos -a 584890 #TP4
+#gh classroom clone student-repos -a 595462 #TP6
+#gh classroom clone student-repos -a 599537 #TP8
 
-gh classroom clone student-repos -a 602302 #TP9
-gh classroom clone student-repos -a 609087 #TP10
-gh classroom clone student-repos -a 614010 #TP11
+#gh classroom clone student-repos -a 602302 #TP9
+#gh classroom clone student-repos -a 609087 #TP10
+#gh classroom clone student-repos -a 614010 #TP11
 
 
--- pull.sh
+++ pull.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-gh classroom pull student-repos -a 572999 #TP1
-gh classroom pull student-repos -a 576011 #TP2
-gh classroom pull student-repos -a 579743 #TP3
-gh classroom pull student-repos -a 584890 #TP4
-gh classroom pull student-repos -a 595462 #TP5
-gh classroom pull student-repos -a 599537 #TP6
+
+gh classroom pull student-repos -a 772080 #TP1
+#gh classroom pull student-repos -a 576011 #TP2
+#gh classroom pull student-repos -a 579743 #TP3
+#gh classroom pull student-repos -a 584890 #TP4
+#gh classroom pull student-repos -a 595462 #TP5
+#gh classroom pull student-repos -a 599537 #TP6
